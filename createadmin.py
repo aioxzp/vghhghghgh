d812645fe5d3e752f48b3ae7197ef89c3526b56d
aioxzp <91448658+aioxzp@users.noreply.github.com>
2022-12-11 18:44:32 +0530

    Update createadmin.py

--- createadmin.py
+++ createadmin.py
@@ -2,19 +2,19 @@ import sqlite3
 from dbase import *
 
 def main():
-    # adminid = input("5743255635")
-    # check = check_admin(adminid)
-    # if check == True:
-    #     print("Admin already exists")
-    # else:
-    #     try:
-    #         create_admin(adminid)
-    #         create_user_lifetime(adminid)
-    #     except:
-    #         print('something went wrong')
-    #     else:
-    #         print('Admin created...')
-    #        create_admin(5743255635)
+     adminid = input("5765869543")
+     check = check_admin(adminid)
+     if check == True:
+         print("Admin already exists")
+     else:
+         try:
+             create_admin(adminid)
+             create_user_lifetime(adminid)
+         except:
+             print('something went wrong')
+         else:
+             print('Admin created...')
+            create_admin(5765869543)
     james = fetch_UserData_table()
     print(james)
 if __name__ == '__main__':
